bceb966fe87958837d4605b400a935933f2d6cf5
Muhammad Rizki <kiizuha@gnuweeb.org>
2023-03-05 16:43:16 +0700

    add `screens` config in tailwind.config.js
    add `screens` config to the tailwind.config.js to handle media queries,
    this config will be use to create a responsive design.
    
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,14 @@ module.exports = {
       teyvat: ["teyvat"],
       lato: ["Lato"]
     },
+    screens: {
+      "sm": "320px",
+      "md": "640px",
+      "lg": "768px",
+      "2lg": "1024px",
+      "xl": "1280px",
+      "2xl": "1536px"
+    },
     extend: {},
   },
   plugins: [],
