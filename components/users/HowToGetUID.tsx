cfd7716cf038cf48372913def44e748c6fe7a526
Muhammad Rizki <kiizuha@gnuweeb.org>
2023-03-07 12:42:02 +0700

    add a <HowToGetUID/> component
    This is component is to display an image how to get Genshin Impact UID.
    
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>

---
+++ components/users/HowToGetUID.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import { useState } from "react";
+
+import example from "@/assets/images/how-to-get-UID.png";
+
+
+const HowToGetUID = () => {
+    const [tutor, setTutor] = useState(false);
+    const displayTutor = () => {
+        setTutor(!tutor)
+    }
+
+    return (
+        <>
+             <div className="font-light text-sm text-[#949494] underline cursor-help mt-1"
+                onClick={displayTutor}>How to get it?</div>
+            {tutor ?
+                <div onClick={displayTutor} className="bg-[#090b18d2] fixed w-[100%] h-[100vh] py-2 flex justify-center items-center">
+                    <Image src={example} alt="Tutor UID" width={1000}/>
+                </div>
+            : <></>}
+        </>
+    );
+}
+
+export default HowToGetUID;
