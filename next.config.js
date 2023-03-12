450aa02a0ea7fcebfcc8f9d31706b0f39c1b98cf
Muhammad Rizki <kiizuha@gnuweeb.org>
2023-03-12 08:55:00 +0700

    add `images` in the next.config.js for using image network
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "enka.network",
+        port: "",
+        pathname: "/ui/**",
+      },
+    ],
+    domains: ["enka.network"]
+  }
 }
 
 module.exports = nextConfig
