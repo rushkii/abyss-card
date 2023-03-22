56742b4cfe8fb3e3a7db4b8f92a2239f874e0f79
Muhammad Rizki <kiizuha@gnuweeb.org>
2023-03-23 01:27:58 +0700

    add a `redirects()` in the `next.config.js`
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/uid',
+        destination: '/',
+        permanent: true,
+      },
+    ]
+  },
   reactStrictMode: true,
   images: {
     remotePatterns: [
