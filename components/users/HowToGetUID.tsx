98688b496fe407b56e0635ce62c0e6e1e3b8b35c
Muhammad Rizki <kiizuha@gnuweeb.org>
2023-03-23 00:16:22 +0700

    adjust margin top from `mt-1` to `mt-3`
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>

--- components/users/HowToGetUID.tsx
+++ components/users/HowToGetUID.tsx
@@ -12,7 +12,7 @@ const HowToGetUID = () => {
 
     return (
         <>
-             <div className="font-light text-sm text-[#949494] underline cursor-help mt-1"
+             <div className="font-light text-sm text-[#949494] underline cursor-help mt-3"
                 onClick={displayTutor}>How to get it?</div>
             {tutor ?
                 <div onClick={displayTutor} className="bg-[#090b18d2] fixed w-[100%] h-[100vh] py-2 flex justify-center items-center">
