91ba02f41ace9c073212fc1aef5b7650831b2f2d
Muhammad Rizki <kiizuha@gnuweeb.org>
2023-03-24 03:49:10 +0700

    Nav: change styling for mobile and change text size.
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -9,18 +9,18 @@ const Nav = () => {
     return (
         <nav className="w-full backdrop-blur border-b-[0.1px] relative z-10 border-b-[#bcc4cf] dark:border-[#cbd5e11a]
                         supports-backdrop-blur:bg-white/60">
-            <div className="px-8 flex sm:justify-center md:justify-between">
-                <Link href="/" title="Abyss" className="sm:text-2xl h-16 flex items-center lg:text-3xl 2xl:text-4xl font-teyvat">
+            <div className="px-8 flex justify-between">
+                <Link href="/" title="Abyss" className="h-16 flex items-center sm:text-3xl 2xl:text-4xl font-teyvat">
                     Abyss
                 </Link>
 
-                <ul className="sm:hidden lg:flex border-sky-600">
+                <ul className="flex border-sky-600">
                     <li className="h-auto">
                         <Link
                             href="/about"
                             title="About"
                             className={`h-16 flex items-center transition-all duration-200 hover:border-white border-b-2
-                            border-transparent font-bold text-2xl
+                            border-transparent font-bold sm:text-xl lg:text-2xl
                             ${currentPath === "/about" ? "border-white" : ""}`}
                         >
                             <div>About</div>
