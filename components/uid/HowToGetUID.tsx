ea2ec5763a9f143a6c5e495c9e09f5816683f277
Muhammad Rizki <kiizuha@gnuweeb.org>
2023-03-28 04:54:02 +0700

    fix the image position on `HowToGetUID.tsx` component
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>

--- components/uid/HowToGetUID.tsx
+++ components/uid/HowToGetUID.tsx
@@ -7,7 +7,7 @@ import example from "@/assets/images/how-to-get-UID.png";
 const HowToGetUID = () => {
     const [tutor, setTutor] = useState(false);
     const displayTutor = () => {
-        setTutor(!tutor)
+        setTutor(!tutor);
     }
 
     return (
@@ -15,8 +15,12 @@ const HowToGetUID = () => {
              <div className="font-light text-sm text-[#949494] underline cursor-help mt-3"
                 onClick={displayTutor}>How to get it?</div>
             {tutor ?
-                <div onClick={displayTutor} className="bg-[#090b18d2] fixed w-[100%] h-[100vh] py-2 flex justify-center items-center">
-                    <Image src={example} alt="Tutor UID" width={1000}/>
+                <div className={`${tutor ? "block": "hidden"} z-[999]`}>
+                    <div className="fixed left-0 top-0 w-full h-full bg-[#000000b0] backdrop-blur-sm flex justify-center items-center"
+                        onClick={displayTutor}
+                    >
+                        <Image src={example} alt="Tutor UID" width={1000} className="block w-[80%] max-w-[800px]"/>
+                    </div>
                 </div>
             : <></>}
         </>
