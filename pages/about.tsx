rushkii/abyss-card

commit 6e97dd56e2a5578c7579237b7a754e7554671e3b

fix: escape `'` to `&#39;`
Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>

pages/about.tsx
@@ -12,7 +12,7 @@ const About = () => {
                 }>
                     <div>
                         <span className="sm:text-2xl xl:text-3xl font-teyvat hover:font-lato hover:font-bold">Abyss</span>
-                        &nbsp;&nbsp;is a website designed to generate a player's profile and transform it into a customized card design.&nbsp;
+                        &nbsp;&nbsp;is a website designed to generate a player&#39;s profile and transform it into a customized card design.&nbsp;
                         I created this website for my portfolio and it is open source on GitHub. You can view it&nbsp;
                         <Link href="https://github.com/rushkii/abyss-card" className="text-sky-500">here</Link>.<br/><br/>
                         <span className="sm:text-2xl xl:text-3xl font-teyvat hover:font-lato hover:font-bold">Abyss</span>

commit 4a0b1ea617d1230d4069d3740730b76a3df15074

recommit: fix: escape `'` to `&#39; for the `pages/about.tsx``
In commit 6e97dd56e2a5, the error was already fixed. However, I messed
it up again while trying to fix the grammar in the `about` content.
I forgot to escape the content again for HTML escape.

Fixes: 6e97dd56e2a5 ("fix: escape `'` to `&#39;`")
Reviewed-by: Muhammad Rizki <kiizuha@gnuweeb.org>
Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>

pages/about.tsx
@@ -12,7 +12,7 @@ const About = () => {
                 }>
                     <div>
                         <span className="sm:text-2xl xl:text-3xl font-teyvat hover:font-lato hover:font-bold">Abyss</span>
-                        &nbsp;&nbsp;website that generates a player's profile and transforms it into a customized card design.&nbsp;
+                        &nbsp;&nbsp;website that generates a player&#39;s profile and transforms it into a customized card design.&nbsp;
                         I developed this website as a part of my portfolio, and it is available as an open-source project on GitHub.&nbsp;
                         You can view it&nbsp;<Link href="https://github.com/rushkii/abyss-card" className="text-sky-500">here</Link>.
                         <br/><br/>